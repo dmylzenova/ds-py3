9f6069e76f762222f56371e2bef5429ed0877f7a
Gregory <g.ivanov@analysiscenter.ru>
2018-05-03 14:53:07 +0300

    added visdom

--- run/run.sh
+++ run/run.sh
@@ -9,7 +9,7 @@ port=${DS_PORT:-8889}
 image=${DS_IMAGE:-analysiscenter1/ds-py3:gpu-1.6}
 
 
-sudo docker run --runtime=nvidia --rm -p ${port}:8888 \
+sudo docker run --runtime=nvidia --rm -p ${port}:8888 -p 8097:8097 -p 5000:5000 -p 9090:9090\
   -v ${notebooks_vol}:/notebooks \
   -v ${config_vol}:/jupyter/config \
   -v ${secret_vol}:/jupyter/secret \
