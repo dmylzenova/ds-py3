75dba0e384827729c2b9108a3e839c467741f1bc
Darima Mylzenova <d.mylzenova@analysiscenter.org>
2019-01-17 13:45:38 +0300

    Update install_nvidia_390.sh
    add k80 download link

--- utils/install_nvidia_390.sh
+++ utils/install_nvidia_390.sh
@@ -7,4 +7,8 @@ wget http://us.download.nvidia.com/XFree86/Linux-x86_64/410.78/NVIDIA-Linux-x86_
 chmod +x NVIDIA-Linux-x86_64-410.78.run
 sudo ./NVIDIA-Linux-x86_64-410.78.run
 
+# for tesla k80 wget http://ru.download.nvidia.com/tesla/410.79/NVIDIA-Linux-x86_64-410.79.run
+# chmod +x NVIDIA-Linux-x86_64-410.79.run
+# sudo ./NVIDIA-Linux-x86_64-410.79.run
+
 nvidia-smi
